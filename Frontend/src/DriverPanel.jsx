430414c619e9295487de92e8b67e2ef3f05d7b9b
Himanshu Yadav <himanshuyadav9015830677@gmail.com>
2026-03-09 03:45:56 +0530

    updated api url for deployment

--- Frontend/src/DriverPanel.jsx
+++ Frontend/src/DriverPanel.jsx
@@ -12,7 +12,7 @@ import {
 } from "react-icons/fa";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("${import.meta.env.VITE_API_URL}");
 
 const Counter = ({ end }) => {
   const [count, setCount] = useState(0);
